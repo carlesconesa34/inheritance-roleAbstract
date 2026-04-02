3bedbba4fff7a4d2c3c2b6e4602d2e323b025feb
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-02 18:00:28 +0200

    Create project structure

---
+++ src/role/Utils.java
@@ -0,0 +1,26 @@
+/*
+ * Utils.java Apr 2, 2026
+ *
+ *
+ * ©Copyright 2026 Carles Conesa <a251158cc@correu.escoladeltreball.org>
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See http://www.gnu.org/licenses/gpl.html for more information.
+ */
+ 
+/**
+ * 
+ */
+
+package role; //Package declaration.
+
+/**
+ * 
+ */
+
+public class Utils {
+	public static void main(String args[]) {
+
+	}
+}
+
