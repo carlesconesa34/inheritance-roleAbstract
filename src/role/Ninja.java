c3e4ac9957f06df041f80035655091f72f0f8263
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-06 15:37:29 +0200

    Add Javadoc comments

--- src/role/Ninja.java
+++ src/role/Ninja.java
@@ -10,12 +10,35 @@
 
 package role;
 
+/**
+ * Concrete implementation of Role representing a Ninja character.
+ *
+ * Each action consumes energy defined in Utils. The action is only
+ * performed when the role is available and has sufficient energy.
+ *
+ * @author Carles Conesa
+ * @version 1.0
+ * @since 2026-04-02
+ */
 public class Ninja extends Role {
 
+    /**
+     * Creates a new Ninja at the specified position on the 2-D plane.
+     *
+     * @param posX initial horizontal position
+     * @param posY initial vertical position
+     */
     public Ninja(float posX, float posY) {
         super(posX, posY);
     }
 
+    /**
+     * Moves the Ninja a fixed number of units along the X axis and
+     * consumes the corresponding energy points.
+     *
+     * The action is only performed if the role is available and has sufficient
+     * energy remaining.
+     */
     @Override
     public void move() {
         if (isAvailable() && getEnergy() >= Utils.ENERGY_MOVE) {
@@ -24,6 +47,13 @@ public class Ninja extends Role {
         }
     }
 
+    /**
+     * Makes the Ninja jump a fixed number of units along the Y axis and
+     * consumes the corresponding energy points.
+     *
+     * The action is only performed if the role is available and has sufficient
+     * energy remaining.
+     */
     @Override
     public void jump() {
         if (isAvailable() && getEnergy() >= Utils.ENERGY_JUMP) {
@@ -32,9 +62,14 @@ public class Ninja extends Role {
         }
     }
 
+    /**
+     * Returns a string representation of this Ninja, prefixed with
+     * the character type and followed by the common Role state.
+     *
+     * @return a string representation of the object
+     */
     @Override
     public String toString() {
         return "Ninja -> " + super.toString();
     }
 }
-
